9a2c99daff412f9683c6773231d9d30d508277fe
billy7175 <68532934+billy7175@users.noreply.github.com>
2021-07-10 01:24:41 +0900

    JS-palindrome

---
+++ JS-Interview/14-palindromes.js
@@ -0,0 +1,13 @@
+/**
+ * Write a function to determine if the supplied string is a palindrome
+ * Eg:  radar,  racecar, kayak, and redder are all palidromes
+ * They are the spelled same backwards and forwards
+ */
+
+ let palindrome = function (word) {
+  
+}
+
+console.log(palindrome('radar')); //true
+console.log(palindrome('redder')); //true
+console.log(palindrome('window')); //false
