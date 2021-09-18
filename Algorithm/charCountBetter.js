e9d6f89d1d3ab28f3760c5cbf9a32946f822f726
billy7175 <68532934+billy7175@users.noreply.github.com>
2021-09-19 04:53:31 +0900

    Update Algorithm

--- Algorithm/charCountBetter.js
+++ Algorithm/charCountBetter.js
@@ -2,15 +2,25 @@ function charCount (str){
   var obj = {};
   for(var i =0; i < str.length; i++){
     var char = str[i].toLowerCase();
-    if(/[a-z0-9]/.test(char)){
-      // if(obj[char] > 0){
-      //   obj[char]++
-      // } else {
-      //   obj[char] = 1
-      // }
+    if(isAlphaNumeric(char)){
       obj[char] = ++obj[char] || 1;
     }
+    // if(/[a-z0-9]/.test(char)){
+    //   obj[char] = ++obj[char] || 1;
+    // }
   }
   return obj;
 }
+
+
+function isAlphaNumeric(char){
+  var code = char.charCodeAt(0);
+  if(!(code > 47 && code < 58) &&
+    !(code > 64 && code < 91) &&
+    !(code > 96 && code < 123)) {
+      return false;
+    }
+    return true;
+}
+
 console.log(charCount('hello world hh'))
