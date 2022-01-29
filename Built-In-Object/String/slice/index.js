f60210a4eb308cf9b375a21459bebe0577a4f287
billy7175 <68532934+billy7175@users.noreply.github.com>
2022-01-29 21:24:16 +0900

    Built-In-Object : substring

--- Built-In-Object/String/slice/index.js
+++ Built-In-Object/String/slice/index.js
@@ -2,8 +2,6 @@ console.log("connected");
 // 1. 'string'.slice('시작 인덱스', 종료 인덱스)
 // 2. 문자열을 반환하지만 기존 문자열을 변환시키지 않는다. which means 변수와 함께 사용. 
 const string = "this is a test sentence";
-
-string.slice(0,3)
 console.log(string.slice(0,3))
 
 const array = ['one', 'twp', 'three','four','five']
---
+++ Built-In-Object/String/substring/index.js
@@ -0,0 +1,10 @@
+// 1. 'string'.substring('시작 인덱스', 종료 인덱스)
+// 2. 문자열을 반환하지만 기존 문자열을 변환시키지 않는다. which means 변수와 함께 사용. 
+// 3. array 사용불가
+
+const string = "this is a test sentence";
+console.log(string.substring(0))
+console.log(string.substring(0,3))
+
+
+
