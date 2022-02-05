348b82d60c51406061b36860cf83aa06774301a6
billy7175 <68532934+billy7175@users.noreply.github.com>
2022-02-06 02:09:00 +0900

    Delete Algorithm Data

--- Algorithm/sort/sort.js
+++
@@ -1,5 +0,0 @@
-function numberCompare(num1, num2){
-  return num1 - num2
-}
-
-console.log([6, 4, 15, 10].sort(numberCompare))
--- Algorithm_02/searchCenterChar.js
+++
@@ -1,6 +0,0 @@
-function solution (str){
-  let answer;
-  let mid = Math.floor(s.length/2)
-}
-let str ='englishmaster'
-console.log(solution(str))
