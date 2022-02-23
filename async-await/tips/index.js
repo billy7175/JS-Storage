8b73292a82ad7081f303efd026a34d329417db4a
LAPTOP-C71E3L3P\Billy <whgus439@gmail.com>
2022-02-24 02:13:52 +0900

    async-await

--- async-await/tips/index.js
+++ async-await/tips/index.js
@@ -6,10 +6,18 @@ function request(num) { // mock HTTP request
   })
 }
 
-request(1).then(res1 => {
-  console.log(res1) // it will print `2` after 1 second
+// request(1).then(res1 => {
+//   console.log(res1) // it will print `2` after 1 second
 
-  request(2).then(res2 => {
-    console.log(res2) // it will print `4` after anther 1 second
-  })
-})
+//   request(2).then(res2 => {
+//     console.log(res2) // it will print `4` after anther 1 second
+//   })
+// })
+
+async function fn() {
+  console.log(1)
+  console.log(await request(1)) // 2
+  console.log(await request(2)) // 4
+  // print `2` and `4` at the same time
+}
+fn()
