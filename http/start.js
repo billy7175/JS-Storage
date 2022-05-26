09ab4aaa7389344069afb3238b614af69779bfa0
billy7175 <whgus439@gmail.com>
2022-05-26 23:18:42 +0900

    complete res html format && get todolist

--- http/start.js
+++ http/start.js
@@ -1,50 +1,92 @@
-
-
 // GET REQUEST
 function getTodos() {
-    console.log('GET Request');
-  }
-  
-  // POST REQUEST
-  function addTodo() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5', {
+        timeout: 5000
+    })
+        .then(res => { showOutput(res) })
+        .catch(err => console.error(err));
+
+}
+
+// POST REQUEST
+function addTodo() {
     console.log('POST Request');
-  }
-  
-  // PUT/PATCH REQUEST
-  function updateTodo() {
+}
+
+// PUT/PATCH REQUEST
+function updateTodo() {
     console.log('PUT/PATCH Request');
-  }
-  
-  // DELETE REQUEST
-  function removeTodo() {
+}
+
+// DELETE REQUEST
+function removeTodo() {
     console.log('DELETE Request');
-  }
-  
-  // SIMULTANEOUS DATA
-  function getData() {
+}
+
+// SIMULTANEOUS DATA
+function getData() {
     console.log('Simultaneous Request');
-  }
-  
-  // CUSTOM HEADERS
-  function customHeaders() {
+}
+
+// CUSTOM HEADERS
+function customHeaders() {
     console.log('Custom Headers');
-  }
-  
-  // TRANSFORMING REQUESTS & RESPONSES
-  function transformResponse() {
+}
+
+// TRANSFORMING REQUESTS & RESPONSES
+function transformResponse() {
     console.log('Transform Response');
-  }
-  
-  // ERROR HANDLING
-  function errorHandling() {
+}
+
+// ERROR HANDLING
+function errorHandling() {
     console.log('Error Handling');
-  }
-  
-  // CANCEL TOKEN
-  function cancelToken() {
+}
+
+// CANCEL TOKEN
+function cancelToken() {
     console.log('Cancel Token');
-  }
-  
+}
+
+function showOutput(res) {
+    console.log(2020202002020020)
+    console.log(res)
+    document.getElementById('res').innerHTML = `
+        <div class="card card-body mb-4">
+            <h5>Status:${res.status}</h5>
+        </div>
+
+        <div class="card mt-3">
+            <div class="card-header">
+                Headers
+            </div>
+            <div class="card-body">
+                <pre>${JSON.stringify(res.headers, null, 2)}</pre>
+            </div>
+        </div>
+
+        <div class="card mt-3">
+            <div class="card-header">
+                Data
+            </div>
+            <div class="card-body">
+                <pre>${JSON.stringify(res.data, null, 2)}</pre>
+            </div>
+        </div>
+
+        <div class="card mt-3">
+            <div class="card-header">
+                Config
+            </div>
+            <div class="card-body">
+                <pre>${JSON.stringify(res.config, null, 2)}</pre>
+            </div>
+        </div>
+    `
+}
+
+
+
 
 
 // Event listeners
@@ -55,7 +97,7 @@ document.getElementById('delete').addEventListener('click', removeTodo);
 document.getElementById('sim').addEventListener('click', getData);
 document.getElementById('headers').addEventListener('click', customHeaders);
 document
-  .getElementById('transform')
-  .addEventListener('click', transformResponse);
+    .getElementById('transform')
+    .addEventListener('click', transformResponse);
 document.getElementById('error').addEventListener('click', errorHandling);
 document.getElementById('cancel').addEventListener('click', cancelToken);
