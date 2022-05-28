aa142dbdd4cdc0c0fbebd0eb3dab3449c5d8d66f
billy7175 <68532934+billy7175@users.noreply.github.com>
2022-05-28 22:29:04 +0900

    simultaneous data

--- http/start.js
+++ http/start.js
@@ -40,6 +40,13 @@ function removeTodo(id = 1) {
 // SIMULTANEOUS DATA
 function getData() {
     console.log('Simultaneous Request');
+    axios
+    .all([
+      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5'),
+      axios.get('https://jsonplaceholder.typicode.com/posts?_limit=5')
+    ])
+    .then(axios.spread((todos, posts) => showOutput(posts)))
+    .catch(err => console.error(err));
 }
 
 // CUSTOM HEADERS
