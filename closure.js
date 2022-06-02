3c47dcf1db6dc50c5584508c46b212e5cda4b017
billy7175 <whgus439@gmail.com>
2022-06-02 23:02:53 +0900

    closure info

--- closure.js
+++ closure.js
@@ -1,3 +1,7 @@
+// 1. A function always has access to the variable environment of the execution context in which it was created.
+// 2. Event after dept execution context is gone.
+// 3. The closure is then basically the variable environment attahced to the function.
+
 const secureBooking = function (){
     let passengerCount = 0;
 
