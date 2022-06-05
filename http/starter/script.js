85e1cc13a39db91e7aa7ae77e49048941e25dfe5
billy7175 <whgus439@gmail.com>
2022-06-05 14:58:04 +0900

    XMLHttpRequest

--- http/starter/script.js
+++ http/starter/script.js
@@ -4,3 +4,33 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const request = new XMLHttpRequest();
+
+request.open('GET', 'https://restcountries.com/v2/name/portugal')
+
+request.send()
+console.log('#request', request)
+console.log(request.responseText);
+
+request.addEventListener('load', function () {
+    console.log('#this', this)
+    console.log(this.responseText)
+    const [ data ] = JSON.parse(this.responseText)
+    const html = `
+    <article class="country">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+        </div>
+    </article>
+    `
+
+    countriesContainer.insertAdjacentHTML('beforeend', html)
+    countriesContainer.style.opacity = 1
+    console.log('#data', data)
+})
